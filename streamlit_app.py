86df2431f07014b68064d59639c6402675fd3b74
snowflake4rupal <110439144+snowflake4rupal@users.noreply.github.com>
2022-08-01 23:02:51 -0400

    Update streamlit_app.py
    Added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Har-Boiled Free Range Egg')
+streamlit.text('Kale, Spinach Smoothie')
+
