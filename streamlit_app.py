c7b797073216eb7028cffc43efc72a972367a063
snowflake4rupal <110439144+snowflake4rupal@users.noreply.github.com>
2022-08-02 06:11:03 -0400

    Update streamlit_app.py
    Added Fruit picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Hard-Boiled Free Range Egg')
@@ -11,4 +11,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')    
+                                
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+                            
+# Display the table on the page.
+streamlit.dataframe(my_fruit_list)                
