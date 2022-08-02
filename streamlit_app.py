2dc5d13b2d7660d37bf113262ba197513c75568b
snowflake4rupal <110439144+snowflake4rupal@users.noreply.github.com>
2022-08-01 23:15:05 -0400

    Update streamlit_app.py
    Added emogies

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Har-Boiled Free Range Egg')
-streamlit.text('Kale, Spinach Smoothie')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Hard-Boiled Free Range Egg')
+streamlit.text('🥗 Kale, Spinach Smoothie')
+streamlit.text('🥑🍞 Avocadeo Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
