decada5619a4eb1c4f0f8910fe5ca18dbe1fa2d5
snowflake4rupal <110439144+snowflake4rupal@users.noreply.github.com>
2022-08-04 16:53:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ def get_fruit_load_list():
        return my_cur.fetchall()
     
 # Add a button to load the fruit
-streamlit.dataframe(my_data_rows)
+# streamlit.dataframe(my_data_rows)
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
