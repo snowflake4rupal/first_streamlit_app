c9eae1e4e2f50b1d6791b94b200dd2d1b92025ba
snowflake4rupal <110439144+snowflake4rupal@users.noreply.github.com>
2022-08-04 15:46:13 -0400

    Update streamlit_app.py
    cleaning up code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+import urllib.error import URLError
 
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -9,7 +13,7 @@ streamlit.text('🥑🍞 Avocadeo Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+# import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')    
                                 
@@ -25,7 +29,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
+# import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # requests.get("https://fruityvice.com/api/fruit/kiwi")
 # streamlit.text(fruityvice_response) #writes how many responses were received
@@ -34,7 +38,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - puts the normalized data in a dataframe (table format) to make it easy to read
 streamlit.dataframe(fruityvice_normalized)
-import snowflake.connector 
+# import snowflake.connector 
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")#("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
