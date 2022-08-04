acb1350f4b79a4f43bcaf5c69eeb2c78a1f4e12d
snowflake4rupal <110439144+snowflake4rupal@users.noreply.github.com>
2022-08-04 16:07:40 -0400

    Update streamlit_app.py
    clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,18 +26,27 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvise API response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+#     streamlit.write('The user entered', fruit_choice)
+except URLError as e:
+  streamlit.error()
+  
 # import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # requests.get("https://fruityvice.com/api/fruit/kiwi")
 # streamlit.text(fruityvice_response) #writes how many responses were received
 
 # write your own comment -flattens the json file
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # write your own comment - puts the normalized data in a dataframe (table format) to make it easy to read
-streamlit.dataframe(fruityvice_normalized)
+
 # import snowflake.connector 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
